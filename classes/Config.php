b9fd16e643d654655e357b7fbffbd9421e0f7be3
markov-algebra <marko.vojtek@gmail.com>
2019-02-07 21:09:57 +0100

    DAtabase class

--- classes/Config.php
+++ classes/Config.php
@@ -2,6 +2,11 @@
 
 class Config{
 
+
+    private function __construct(){}
+        private function __clone(){
+        
+        }
 //defoltna vrijednost je null ako netko zove get da ne pukneS
        
 public static function get($path = null){
